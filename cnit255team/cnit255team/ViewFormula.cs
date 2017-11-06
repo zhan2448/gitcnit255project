245a827429d384e1b5e63514f38998fe7b760416
zhan2448 <zhan2448@purdue.edu>
2017-11-06 15:42:09 -0500

    update

---
+++ cnit255team/cnit255team/ViewFormula.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace cnit255team
+{
+    public partial class ViewFormula : UIView
+    {
+        public ViewFormula (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
