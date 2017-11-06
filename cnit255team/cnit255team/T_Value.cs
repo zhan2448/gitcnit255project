1b1b33bb8d161f36630037a7c748e052bc01ca12
zhan2448 <zhan2448@purdue.edu>
2017-11-06 18:23:24 -0500

    puah

--- cnit255team/cnit255team/T_Value.cs
+++ cnit255team/cnit255team/T_Value.cs
@@ -15,9 +15,23 @@ namespace cnit255team
         {
             SetPopulation_Mean(xPop_Mean);
             SetDataSet(xDataSet);
+            double Mean = xDataSet.GetSample_Mean();
+            double h_mean = xPop_Mean;
+            double sd = xDataSet.GetSample_SD(); 
+            double sampleN = xDataSet.GetSample_Size();
+
+            {
+
+                double temp = Mean - h_mean;
+                double temp2 = sd / Math.Sqrt(sampleN);
+                double temp3 = temp / temp2;
+                temp3 = Math.Abs(temp3);
+                temp3 = Math.Round(temp3, 4);
+
+                Value = temp3;
+            }
         }
-
-
+       
         public void SetValue(double xValue) { Value = xValue; }
         public void SetPopulation_Mean(double xPop_Mean) { Population_Mean = xPop_Mean; }
         public void SetDataSet(DataSet xDataSet) { DataSet = xDataSet; }
