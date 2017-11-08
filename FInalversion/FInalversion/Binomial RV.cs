7f081ee74323e5abfbcb05ddf6bad3b131cf1269
zhan2448 <zhan2448@purdue.edu>
2017-11-08 16:09:17 -0500

    icon

--- FInalversion/FInalversion/Binomial RV.cs
+++ FInalversion/FInalversion/Binomial RV.cs
@@ -18,12 +18,11 @@ namespace FinalVersion
             InputTypes.Add("s");
         }
 
+
         public void SetN(int xN) { Values[0] = (int)xN; }
         public void SetP(double xP) { Values[1] = (double)xP; }
-
         public int GetN() { return (int)Values[0]; }
         public double GetP() { return (double)Values[1]; }
-
         public void CalculateValues(int xN, double xP) {
             SetN(xN);
             SetP(xP);
