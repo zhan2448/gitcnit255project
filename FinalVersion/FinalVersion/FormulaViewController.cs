d0ca253f066955d8e7491ffb930bd75e4fbaf357
zhan2448 <zhan2448@purdue.edu>
2017-12-09 18:41:44 -0500

    1109

---
+++ FinalVersion/FinalVersion/FormulaViewController.cs
@@ -0,0 +1,23 @@
+using Foundation;
+using System;
+using UIKit;
+
+using ObjCRuntime;
+namespace FinalVersion
+{
+    public partial class FormulaViewController : UIViewController
+    {
+
+       
+        public FormulaViewController (IntPtr handle) : base (handle)
+        {
+        }
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var a = new CalculationTableControl();
+        }
+       
+       
+    }
+}
