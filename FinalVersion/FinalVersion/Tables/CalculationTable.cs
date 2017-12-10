6d44a5a11f8232ce9d8a75abc31f9a734fa4717f
zhan2448 <zhan2448@purdue.edu>
2017-12-09 19:43:23 -0500

    p

---
+++ FinalVersion/FinalVersion/Tables/CalculationTable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FinalVersion.Tables
+{
+    public class CalculationTable
+    {
+        public CalculationTable()
+        {
+        }
+    }
+}
---
+++ FinalVersion/FinalVersion/ViewController/FormulaViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace FinalVersion
+{
+    public partial class FormulaViewController : UIViewController
+    {
+        public FormulaViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
