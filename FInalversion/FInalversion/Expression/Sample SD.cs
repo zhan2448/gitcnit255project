ad6ffe1f0ffcf11abe33a66b13458bda5612e991
ArtemIl <ilumzhinov@gmail.com>
2017-12-11 22:39:01 -0500

    stupid comments are deleted

--- FInalversion/FInalversion/Expression/Sample SD.cs
+++ FInalversion/FInalversion/Expression/Sample SD.cs
@@ -11,7 +11,6 @@ namespace FinalVersion
             "s"
         )
         {
-<<<<<<< HEAD
             if (isPrimitive)
                 return;
 
@@ -20,14 +19,6 @@ namespace FinalVersion
             secondSegment[0] = new DataSet();
 
             SubExpressions.Add(secondSegment);
-=======
-            Title="Sample S.D.";
-            Sign = "σ";
-            InputType = "s";
-            Expression[] firstSegment = new Expression[1];
-            firstSegment[0] = new DataSet();
-            SubExpressions = new Expression[1][] { firstSegment };
->>>>>>> 4005c016f7250a03ece18921b5c2900849737df7
         }
     }
 }
